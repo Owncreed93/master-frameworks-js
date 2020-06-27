d16193f45d5df1b39777e6e9f831e7184c56e604
owncreed93 <christiantarifer@gmail.com>
2020-06-27 16:29:20 -0500

    terminado backend

--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -1,7 +1,12 @@
 'use strict'
 
 let validator = require('validator')
+let fs = require('fs')
+let path = require('path')
+
 let Article = require('../models/article');
+const { exists } = require('../models/article');
+const article = require('../models/article');
 
 let controller = {
 
@@ -198,6 +203,173 @@ let controller = {
             });
         }
         
+    },
+
+    delete: (req, res) => {
+
+        // * GET THE ID OF THE URL
+        let articleID = req.params.id
+
+        // * FIND AND DELETE THE ARTICLE
+        Article.findOneAndDelete({_id: articleID}, (err, articleRemoved) => {
+
+            if( err ){
+                return res.status(500).send({
+                    status:'error',
+                    message: 'Error al borrar!!!'
+                });
+            }
+
+            if( !articleRemoved ){
+                return res.status(404).send({
+                    status:'error',
+                    message: 'No se ha borrado el artículos, posiblemente no exista!!!'
+                });
+            }
+
+            return res.status(200).send({
+                status:'success',
+                article: articleRemoved
+            });
+
+        })
+        
+    },
+
+    upload: (req, res) => {
+        // * SET UP MODULE CONNECT-MULTIPARTY
+        
+        // * GET THE FILE FROM THE REQUEST
+        let file_name = 'Imagen no subida'
+
+        if ( !req.files ){
+            return res.status(404).send({
+                status:'success',
+                fichero: file_name
+            });
+        }
+        // * GET THE NAME AND THE EXTENSION
+        let file_path = req.files.file0.path;
+
+        console.log(file_path)
+        /** 
+         * WINDOWS
+         * let file_split = file_path.split('\\')
+         * LINUX O MAC
+         * let file_split = file_path.split('/')
+        */
+        let file_split = file_path.split('/')
+        file_name = file_split[2]
+
+        //  * GET THE EXTENSION
+        let extension_split = file_name.split('\.')
+        let file_ext = extension_split[1]
+
+        // * CHECK THE EXTENSION (ONLY IMAGES, IF INVALID DELETE THE FILE)
+        if ( file_ext != 'png' && file_ext != 'jpg' && file_ext != 'jpeg' && file_ext != 'gif') {
+
+            // * BORRAR EL FICHERO SUBIDO
+            fs.unlink(file_path, (err) => {
+
+                return res.status(200).send({
+                    status:'error',
+                    message: 'La extensión de la imágen no es válida'
+                });
+            })
+
+        } else {
+            // * GET THE ARTICLE'S ID BY THE URL
+            let articleID = req.params.id
+
+
+            // * LOOKF FOR THE ARTICLE, ASIGN NAME TO THE IMAGE AND UPDATE IT 
+            Article.findOneAndUpdate({_id: articleID }, {image: file_name}, {new: true}, (err, articleUpdated) => {
+                if ( err ){
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'La imagen no se actualizó'
+                    })
+                }
+
+                if ( !articleUpdated ){
+                    return res.status(404).send({
+                        status: 'err',
+                        message: 'Articulo no encontrado'
+                    })
+                }
+
+                return res.status(200).send({
+                    status:'success',
+                    fichero: req.files,
+                    article: articleUpdated
+                })
+
+            })
+            
+        }
+ 
+    }, // * End upload file
+
+    getImage: (req, res) => {
+
+        let file = req.params.image
+
+        let path_file = `./upload/articles/${file}`
+
+        fs.exists(path_file, (exists) => {
+
+            if( exists ) {
+                return res.sendFile(path.resolve(path_file))
+            } else {
+                return res.status(404).send({
+                    status:'error',
+                    message: 'La imagen no existe !!!'
+                })
+            }
+
+        })
+
+    },
+
+    search: (req, res) => {
+        // * GET THE DATA TO SEARCH
+        let searchString = (req.params.search).trim();
+
+        // * FIND BY OR
+        Article.find({
+            "$or": [
+                {"title" : {"$regex": searchString, "$options": "i"} },
+                {"content" : {"$regex": searchString, "$options": "i"} },
+            ]
+        })
+        .sort([['date', 'descending']])
+        .exec( (err, articles) => {
+
+            if( err ){
+                return res.status(500).send({
+                    status:'error',
+                    message: 'No hay articulos'
+                })
+            }
+
+            if( !articles || articles.length <= 0){
+                return res.status(404).send({
+                    status:'Not found',
+                    message: 'No hay articulos asociados a la búsqueda'
+                })
+            }
+
+            return res.status(200).send({
+                status:'success',
+                articles
+            })
+
+
+        })
+
+        
+
+
     }
 
 } // end controller
--- Backend/routes/articles.js
+++ Backend/routes/articles.js
@@ -6,6 +6,12 @@ let ArticleController = require('../controllers/article')
 
 let router = express.Router()
 
+let multipart = require('connect-multiparty')
+
+// * MIDDELWARES
+// * DEAL WITH IMAGES 
+let md_upload = multipart({ uploadDir: './upload/articles' })
+
 // * TEST ROUTES
 router.post('/datos-curso', ArticleController.datosCurso)
 router.get('/test-de-controlador', ArticleController.test)
@@ -15,4 +21,9 @@ router.post('/save', ArticleController.save)
 router.get('/articles/:last?', ArticleController.getArticles)
 router.get('/article/:id', ArticleController.getArticle)
 router.put('/article/:id', ArticleController.update)
+router.delete('/article/:id', ArticleController.delete)
+router.post('/upload-image/:id', md_upload, ArticleController.upload)
+router.get('/get-image/:image', ArticleController.getImage)
+router.get('/search/:search', ArticleController.search)
+
 module.exports = router
