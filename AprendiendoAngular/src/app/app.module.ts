e411bd6d355949415c09d0e7e40304c52b0d3454
owncreed93 <christiantarifer@gmail.com>
2020-06-28 02:59:24 -0500

    anadido rutas

--- AprendiendoAngular/src/app/app.module.ts
+++ AprendiendoAngular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { routing, appRoutingProviders } from './app.routing';
 
 import { AppComponent } from './app.component';
 import { MiComponenteComponent } from './components/mi-componente/mi-componente.component';
@@ -9,6 +10,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { ErrorComponent } from './components/error/error.component';
+
 
 @NgModule({
   declarations: [
@@ -20,11 +27,17 @@ import { FooterComponent } from './components/footer/footer.component';
     SliderComponent,
     SidebarComponent,
     FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    PaginaComponent,
+    FormularioComponent,
+    ErrorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
   ],
-  providers: [],
+  providers: [ appRoutingProviders ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
