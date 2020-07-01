7285df6d3e4da71ce5361cf39774b6344ca0679f
owncreed93 <christiantarifer@gmail.com>
2020-07-01 15:16:45 -0500

    trabajando con ngStyle, ngClass y @Input

--- AprendiendoAngular/src/app/app.component.ts
+++ AprendiendoAngular/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Curso de Angular de Víctor Robles Web';
+  public homeText = 'Bienvenido al curso de Angular con Víctor Robles de victorroblesweb.es 2';
 }
