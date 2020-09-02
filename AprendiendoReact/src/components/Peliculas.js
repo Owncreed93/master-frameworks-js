d2cb21353aa6518b41ffab437124eaa0cbddb30d
owncreed93 <christiantarifer@gmail.com>
2020-09-01 20:22:41 -0500

    Learning about life cycles, conditional on React

--- AprendiendoReact/src/components/Peliculas.js
+++ AprendiendoReact/src/components/Peliculas.js
@@ -1,25 +1,119 @@
 import React, { Component } from 'react';
+import Pelicula from './Pelicula';
 
 class Peliculas extends Component {
 
-    state = {
+    state = { }
 
-        peliculas: [
-            { titulo: 'Batman vs Superman', image: 'https://pics.filmaffinity.com/Batman_v_Superman_El_amanecer_de_la_Justicia-113098552-large.jpg' },
+    cambiarTitulo = () => {
+
+        let { peliculas } = this.state;
+
+        // let random = Math.floor(Math.random() * 3);
+
+        peliculas[0].titulo = 'Batman Begins'
+
+        this.setState({
+
+            peliculas: peliculas
+
+        })
+    }
+
+    favorita = (pelicula, indice) => {
+
+        console.log('Favorita Marcada');
+        console.log(pelicula, indice);
+        console.log(indice);
+
+        this.setState({
+
+            favorita: pelicula
+
+        })
+
+    }
+
+    componentWillMount() {
+        alert('The component will be set');
+        this.setState({ 
+            peliculas: [
+            { titulo: 'Batman vs Superman', image: 'https://vignette.wikia.nocookie.net/dcextendeduniverse/images/b/bd/Batman_v_Superman_Dawn_of_Justice.png/revision/latest/top-crop/width/360/height/450?cb=20160217012051&path-prefix=es' },
             { titulo: 'Gran Torino', image: 'https://assets.puzzlefactory.pl/puzzle/265/858/original.jpg' },
             { titulo: 'Looper', image: 'https://images-na.ssl-images-amazon.com/images/I/719PHksoVaL._SL1500_.jpg' }
-        ],
+             ],
 
-        nombre: 'Christian Tarifeño Ramirez'
+            nombre: 'Christian Tarifeño Ramirez',
+
+            favorita: {}
+        })
+        
+    }
+
+    componentDidMount() {
+
+        alert('The component has been set');
+
+    }
+
+    componentWillUnmount() {
+
+        alert('This component will be dismounted.');
 
     }
 
     render () {
+        let pStyle = {
+            background: 'green',
+            color: 'white',
+            padding: '10px'
+        }
+
+        let favorita;
+
+        if( this.state.favorita.titulo ) {
+
+            favorita = (
+
+                <p className="favorita" style={pStyle} >
+                    <strong>La pelicula favorita es : </strong>
+                    <span>{this.state.favorita.titulo}</span>
+                </p>
+
+            );
+
+        } else {
+
+            favorita = (
+
+                <p>No hay pelicula favorita</p>
+
+            )
+
+        }
+
         return (
 
             <div id="content" className="peliculas">
                 <h2 className="subheader">Peliculas</h2>
                 <p>Selección de las pel&iacute;culas de {this.state.nombre}</p>
+                <p><button onClick={this.cambiarTitulo}>Cambiar t&iacute;tulo</button></p>
+
+                {/* {this.state.favorita.titulo ? (  
+
+                        <p className="favorita" style={pStyle} >
+                        <strong>La pelicula favorita es : </strong>
+                        <span>{this.state.favorita.titulo}</span>
+                        </p>
+
+                    ) : ( 
+                        <p>No hay pelicula favorita</p>
+                    )
+                    
+                } */}
+
+                {favorita}
+                
 
                 {/** CREATE PELICULA COMPONENT */}
 
@@ -27,19 +121,11 @@ class Peliculas extends Component {
                     {this.state.peliculas.map( (pelicula, i) => {
 
                         return(
-                            <article className="article-item" id="article-template" key={i}>
-                                <div className="image-wrap">
-                                    <img src={pelicula.image} alt={pelicula.titulo} />
-                                </div>
-                
-                                <h2>{pelicula.titulo}</h2>
-                                <span className="date">
-                                    Hace 5 min
-                                </span>
-                                <a href="#">Leer m&aacute;s</a>
-
-                                <div className="clearfix"></div>
-                            </article>
+                            <Pelicula key={i} 
+                                      pelicula={pelicula}
+                                      indice={i}
+                                      marcarFavorita= {this.favorita}
+                            />
                         )
 
                     })}
