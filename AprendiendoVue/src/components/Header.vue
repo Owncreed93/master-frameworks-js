23b96a7c284ee33b9e573a71e9c6a336e15fdc2a
owncreed93 <christiantarifer@gmail.com>
2020-10-06 12:05:53 -0500

    Added Vue project, reviewing vue scafolding

---
+++ AprendiendoVue/src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header id="header">
+
+        <div class="center">
+            <!-- LOGO -->
+            <div id="logo">
+                <img src="../assets/logo.png" alt="logotipo" class="app-logo">
+                <span id="brand"><strong>Curso</strong>React</span>
+            </div>
+
+            <!-- MENU -->
+            <nav id="menu">
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="formulario.html">Formulario</a></li>
+                    <li><a href="#">P&aacute;gina 1</a></li>
+                    <li><a href="#">P&aacute;gina 2</a></li>
+                </ul>
+            </nav>
+
+            <!-- LIMPIAR FLOTADOS -->
+            <div class="clearfix"></div>
+        </div>
+        
+    </header>
+</template>
+
+<script>
+export default {
+    name:'HeaderComponent'
+}
+</script>
